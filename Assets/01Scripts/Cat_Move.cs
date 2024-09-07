23d9ec80a8aa31d39f4bbae917910dfceb3e62f1
jms0721 <160107058+jms0721@users.noreply.github.com>
2024-09-07 23:08:48 +0900

    Cat move
    고양이가 오른쪽으로 나감

--- Assets/01Scripts/Cat_Move.cs
+++ Assets/01Scripts/Cat_Move.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Cat_Move : MonoBehaviour
 {
     Rigidbody2D rigid;
+    Animator anim;
     public int nextMove;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        Think();
+        anim = GetComponent<Animator>();
 
         // 5초 후 실행
         Invoke("Think", 5);
@@ -22,22 +23,27 @@ public class Cat_Move : MonoBehaviour
         rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
 
         //지형 확인
-        //Vector2 frontVec = new Vector2(rigid.position.x)
-        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.5f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
 
-        if (rayHit.collider != null)
+        if (rayHit.collider == null)
         {
-            if (rayHit.distance < 0.5f)
-                anim.SetBool("isJumping", false);
+            //Debug.Log("경고! 이 앞은 낭떨어지"); //앞에 낭떨어지가 있다면 디버깅
+            nextMove *= 0;
+            CancelInvoke();
+            Invoke("Think", 5);
         }
     }
 
     //재귀함수(자기 함수가 자신을 한번더 호출)
+    //고양이 움직임
     void Think()
     {
         nextMove = Random.Range(0, 2);
 
         Invoke("Think", 5);
+
+        anim.SetInteger("WalkSpeed", nextMove);
     }
 }

4ccfc40bf8add277ceb20d478c968c4958ab82bd
jms0721 <160107058+jms0721@users.noreply.github.com>
2024-09-16 18:00:33 +0900

    스테이지 변경1
    GameManger.cs-36번 줄 오류

--- Assets/02Scripts/GameManger.cs
+++ Assets/02Scripts/GameManger.cs
@@ -10,13 +10,32 @@ public class GameManger : MonoBehaviour
     public int totalPoint;
     public int stagePoint;
     public int stageIndex;
-    public GameManger[] Stages;
+    public Butler_Move butler_Move;
+    public GameObject[] Stages;
 
     public void NextStage()
     {
-        stageIndex++;
-
+        //스테이지 변경
+        if(stageIndex < Stages.Length - 1)
+        {
+            Stages[stageIndex].SetActive(false);
+            stageIndex++;
+            Stages[stageIndex].SetActive(true);
+        }
+        else
+        {
+            //게임 완료
+            Time.timeScale = 0;
+            Debug.Log("게임 완료");
+        }
         totalPoint += stagePoint;
         stagePoint = 0;
+
     }
+
+    //void ButlerReposition()
+    //{
+    //    butler_Move.transform.position = new Vector3(0, 0, -1);
+    //    butler_Mo
+    //}
 }
